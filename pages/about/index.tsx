c4a3c48cf31a50eb980b3ae2771d51e92ce33531
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-01-18 05:09:40 -0300

    avance about

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,31 +1,42 @@
 "use client"
 
-import { back } from '@/assets'
+import { about, back } from '@/assets'
 import Curve from '@/lib'
+import { ArrowLeft } from 'lucide-react'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 const page = () => {
 
+    const router = useRouter()
+
     return (
-        <Curve backgroundColor={"#ffffff"}>
-        <div className='bg-about h-screen w-screen'>
-            <div className='p-16 m-30 flex flex-col'>
-                <h1 className='text-white font-semibold text-5xl'>
-                    <i>Lorem ipsum dolor sit</i>
-                </h1>
-                <p className='text-white text-3xl mt-2 mb-2'>texty kuagf kuajd af</p>
-                <p className='text-white text-2xl w-1/2'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam omnis blanditiis quas? 
-                    Nemo beatae nihil ipsum.Blanditiis error neque laborum rem atque dolore, 
-                    numquam nesciunt labore quo optio corrupti vero?
-                </p>
-                <p className='text-white text-2xl w-1/2 mt-2'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam omnis blanditiis quas? 
-                    Nemo beatae nihil ipsum.Blanditiis error neque laborum rem atque dolore, 
-                    numquam nesciunt labore quo optio corrupti vero?
-                </p>
+        // <Curve backgroundColor={"#ffffff"}>
+        // </Curve>
+        <div className='h-screen w-screen flex items-center justify-center'>
+            {/* <Image src={about} alt='logo' className='h-[400px] w-[400px] ' /> */}
+            <ArrowLeft
+                className='absolute top-10 left-10 w-7 h-7 cursor-pointer'
+                onClick={() => router.back()}
+            />
+            <div className='bg-black h-[600px] w-[1200px] flex relative'>
+                <div className='absolute -left-3 -top-3 h-[60px] w-[60px] bg-white rounded-full' />
+                <div className='absolute -left-3 -bottom-3 h-[60px] w-[60px] bg-white rounded-full' />
+
+                <div className='h-full w-1/2 overflow-hidden relative inset-x-1/4'>
+                    <h1 className='text-white ml-10 mt-10 font-semibold italic text-5xl'>Juan Ignacio Wilvers</h1>
+                    <p className='text-white ml-10 mt-1 text-3xl'>FrontEnd Developer</p>
+                    <p className='text-white ml-10 mt-2 text-2xl'>
+                        Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. A architecto saepe est magni quod rerum, omnis optio
+                        debitis molestidas distinctio error. Culpa quibusdam architecto maiores animi
+                        reiciendis in officiis quae.
+                    </p>
+                </div>
+
+                <div className='absolute -right-3 -top-3 h-[60px] w-[60px] bg-white rounded-full' />
+                <div className='absolute -right-3 -bottom-3 h-[60px] w-[60px] bg-white rounded-full' />
             </div>
         </div>
-        </Curve>
     )
 }
 
