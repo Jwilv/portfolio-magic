3814993fec85028be672172f388394549df7455b
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-01-17 21:39:32 -0300

    update

--- assets/index.ts
+++ assets/index.ts
@@ -1,3 +1,9 @@
 import logo from './logo.jpg'
+import back from './back.jpg'
+import about from './ss.jpg'
 
-export { logo }
+export {
+    logo,
+    about,
+    back
+}
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,14 +1,30 @@
 "use client"
 
+import { back } from '@/assets'
 import Curve from '@/lib'
-import { useRouter } from 'next/navigation'
-import React, { useEffect } from 'react'
-
+import Image from 'next/image'
 const page = () => {
 
     return (
         <Curve backgroundColor={"#ffffff"}>
-            <div>page</div>
+        <div className='bg-about h-screen w-screen'>
+            <div className='p-16 m-30 flex flex-col'>
+                <h1 className='text-white font-semibold text-5xl'>
+                    <i>Lorem ipsum dolor sit</i>
+                </h1>
+                <p className='text-white text-3xl mt-2 mb-2'>texty kuagf kuajd af</p>
+                <p className='text-white text-2xl w-1/2'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam omnis blanditiis quas? 
+                    Nemo beatae nihil ipsum.Blanditiis error neque laborum rem atque dolore, 
+                    numquam nesciunt labore quo optio corrupti vero?
+                </p>
+                <p className='text-white text-2xl w-1/2 mt-2'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam omnis blanditiis quas? 
+                    Nemo beatae nihil ipsum.Blanditiis error neque laborum rem atque dolore, 
+                    numquam nesciunt labore quo optio corrupti vero?
+                </p>
+            </div>
+        </div>
         </Curve>
     )
 }
