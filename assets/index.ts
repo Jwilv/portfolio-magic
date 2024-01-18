0158fb1061275721e8d374c3a958c30b931fb8ba
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-01-18 18:24:37 -0300

    responsive about

--- assets/index.ts
+++ assets/index.ts
@@ -1,11 +1,9 @@
 import logo from './logo.jpg'
 import back from './back.jpg'
 import about from './ss.jpg'
-import github from './github.png'
 
 export {
     logo,
     about,
     back,
-    github
 }
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import { about, back, github } from '@/assets'
+import { about, back } from '@/assets'
 import Curve from '@/lib'
-import { ArrowLeft, Download, Linkedin, User } from 'lucide-react'
+import { ArrowLeft, Download, Github, Linkedin, User } from 'lucide-react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 const page = () => {
@@ -14,15 +14,22 @@ const page = () => {
 
             <div className='h-screen w-screen flex items-center justify-center'>
                 <ArrowLeft
-                    className='max-w-7 max-h-7 text-Black cursor-pointer m-10 relative'
+                    className='max-w-7 max-h-7 lg:m-10 lg:top-10 lg:left-10 absolute lg:text-black 
+                    cursor-pointer text-white top-10 left-6'
                     onClick={() => router.back()}
                 />
-                <div className='bg-black h-[600px] w-[1200px] flex'>
+                <div className='bg-black h-full w-full lg:h-[600px] lg:w-[1200px] flex justify-center'>
 
-                    <div className='h-full w-1/2 overflow-hidden  inset-x-1/4'>
-                        <h1 className='text-white ml-10 mt-10 font-semibold italic text-5xl'>Juan Ignacio Wilvers</h1>
-                        <p className='text-white ml-10 mt-1 text-3xl'>FrontEnd Developer</p>
-                        <p className='text-white ml-10 mt-2 text-2xl'>
+                    <div className='h-full fg:w-1/2 w-full overflow-hidden'>
+                        <h1 className='text-white ml-10 lg:mt-10 font-semibold italic fg:text-5xl
+                            text-3xl mt-20'
+                        >
+                            Juan Ignacio Wilvers
+                        </h1>
+                        <p className='text-white ml-10 mt-1 lg:text-3xl text-2xl'>
+                            FrontEnd Developer
+                        </p>
+                        <p className='text-white ml-10 mt-2 lg:text-2xl text-xl mr-3'>
                             Lorem ipsum dolor sit amet
                             consectetur adipisicing elit. A architecto saepe est magni quod rerum, omnis optio
                             debitis molestidas distinctio error. Culpa quibusdam architecto maiores animi
@@ -36,40 +43,36 @@ const page = () => {
                                 target="_blank"
                                 className='flex'
                             >
-                                <Image
-                                    src={github}
-                                    alt='github'
-                                    className='w-7 h-7 mr-1 text-white invertir-color'
-                                />
+                                <Github className='w-7 h-7 mr-1' />
                                 GitHub
                             </a>
                         </button>
 
-                        {/* <button
-                        className='text-white ml-10 mt-10 border p-2 hover:bg-white hover:text-black transition text-lg'
-                    >
-                        <a
-                            href="https://github.com/juaniwilvers"
-                            target="_blank"
-                            className='flex'
+                        <button
+                            className='text-white ml-10 mt-10 border p-2 hover:bg-white hover:text-black transition text-lg'
                         >
-                            <User className='w-7 h-7 mr-1' />
-                            Linkedin
-                        </a>
-                    </button>
+                            <a
+                                href="https://github.com/juaniwilvers"
+                                target="_blank"
+                                className='flex'
+                            >
+                                <User className='w-7 h-7 mr-1' />
+                                Linkedin
+                            </a>
+                        </button>
 
-                    <button
-                        className='text-white ml-10 mt-10 border p-2 hover:bg-white hover:text-black transition text-lg'
-                    >
-                        <a
-                            href="https://github.com/juaniwilvers"
-                            target="_blank"
-                            className='flex'
+                        <button
+                            className='text-white ml-10 mt-10 border p-2 hover:bg-white hover:text-black transition text-lg'
                         >
-                            <Download className='w-7 h-7 mr-1' />
-                            Curriculum
-                        </a>
-        //             </button> */}
+                            <a
+                                href="https://github.com/juaniwilvers"
+                                target="_blank"
+                                className='flex'
+                            >
+                                <Download className='w-7 h-7 mr-1' />
+                                Curriculum
+                            </a>
+                        </button>
                     </div>
                 </div>
             </div>
