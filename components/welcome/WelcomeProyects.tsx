47715887dc0fda2aba0b4ebd1542cba822cff239
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-01-20 22:58:19 -0300

    WelcomeProyects

--- components/welcome/WelcomeProyects.tsx
+++ components/welcome/WelcomeProyects.tsx
@@ -1,7 +1,13 @@
+import { Pyramid } from "lucide-react"
 
 
 export const WelcomeProyects = () => {
     return (
-        <div>welcomeProyects</div>
+        <div className="flex items-center justify-center w-screen h-36">
+            <div className="flex -mb-14">
+                <Pyramid className="h-20 w-20" />
+                <h1 className="m-4 text-4xl font-semibold">Mis proyectos</h1>
+            </div>
+        </div>
     )
 }
--- pages/proyects/index.tsx
+++ pages/proyects/index.tsx
@@ -1,20 +1,20 @@
 import { FilterProyects } from "@/components/FilterProyects"
 import { ProyectsList } from "@/components/proyect/ProyectsList"
 import { Separator } from "@/components/ui/separator"
-import { WelcomeProyects } from "@/components/welcome/welcomeProyects"
+import { WelcomeProyects } from "@/components/welcome/WelcomeProyects"
 import Curve from "@/lib"
 
 
 const ProyectsPage = () => {
     return (
-        // <Curve backgroundColor={"#ffffff"}>
-        // </Curve>
-        <>
+        <Curve backgroundColor={"#ffffff"}>
+
             <WelcomeProyects />
             <FilterProyects />
             <Separator className="bg-zinc-400" />
             <ProyectsList />
-        </>
+
+        </Curve>
     )
 }
 
