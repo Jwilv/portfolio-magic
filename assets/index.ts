c5fb2c54f4d3b1b00f31e98392dad64e5a9957be
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-04-02 16:17:28 -0300

    responsive presentation

--- assets/index.ts
+++ assets/index.ts
@@ -1,5 +1,4 @@
-import Component1 from './Component1.png'
-import Component2 from './Component2.png'
+import Scroll from './scroll.png'
 import logo from './logo.svg'
 import BlackBg from './black-bg.jpeg'
 import dslite from './proyects/dsLite.png'
@@ -14,8 +13,7 @@ import ledix from './experience/ledix.png'
 
 
 export {
-    Component1,
-    Component2,
+    Scroll,
     logo,
     BlackBg,
     dslite,
--- components/screens/Presentation.tsx
+++ components/screens/Presentation.tsx
@@ -1,29 +1,27 @@
+import { Scroll } from '@/assets'
 import { Github, Linkedin } from 'lucide-react'
-import { Component1, Component2 } from '@/assets'
 import Image from 'next/image'
 
 export const Presentation = () => {
     return (
         <div className='flex flex-col justify-center h-screen w-screen relative'>
-            <span className='ml-[180px] text-[#909090] mt-[-150px] font-semibold'>Hola a todos!</span>
-            <h1 className='text-9xl font-bold w-[890px] ml-[180px]'>Wilvers Juan<br /> Front End Dev.</h1>
+            <span className='md:ml-[180px] text-[#909090] mt-[-150px] font-semibold ml-10'>Hola a todos!</span>
+            <h1 className='2xl:text-9xl lg:text-8xl font-bold md:w-[890px] md:ml-[180px] lg:bg-[#E8E7EA] md:text-7xl sm:text-6xl text-5xl ml-10'>Wilvers Juan<br /> Front End Dev.</h1>
             <Image
-                src={Component1}
+                src={Scroll}
                 alt="Picture of the author"
-                className='absolute top-[30%] right-[40%]'
-            />
-            <Image
-                src={Component2}
-                alt="Picture of the author"
-                className='absolute top-[32.3%] right-[42%]'
+                className='absolute 2xl:top-[30%] 2xl:right-[40%] lg:top-[30%] lg:left-[60%] top-[15%] left-0 right-0 m-auto'
             />
 
-
-            <div className='border-[#E8E7EA] rounded-full border-2 w-12 h-12 flex justify-center items-center absolute left-[46%] top-[70%]'>
+            <div 
+            className='border-[#E8E7EA] rounded-full border-2 w-12 h-12 flex justify-center items-center absolute left-[30%] top-[70%]
+            md:left-[40%]'>
                 <Github className='w-7 h-7' />
             </div>
 
-            <div className='border-[#E8E7EA] rounded-full border-2 w-12 h-12 flex justify-center items-center absolute right-[46%] top-[70%]'>
+            <div 
+            className='border-[#E8E7EA] rounded-full border-2 w-12 h-12 flex justify-center items-center absolute right-[30%] top-[70%]
+             md:right-[40%]'>
                 <Linkedin className='w-7 h-7' />
             </div>
 
